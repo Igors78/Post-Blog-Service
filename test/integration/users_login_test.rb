8e6ab1f028bfc584d1c43210a34cf274772954fe
Igors78 <oleinikovsigors@gmail.com>
2020-12-18 15:50:29 +0200

    Fix test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -9,7 +9,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'sessions/new'
     post login_path, params: { session: { email: @user.email,
                                           password: 'invalid' } }
-    assert_not is_logged_in?
+    assert_not logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
@@ -20,7 +20,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email: @user.email,
                                           password: 'password' } }
-    assert is_logged_in?
+    assert logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
@@ -28,7 +28,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
     delete logout_path
-    assert_not is_logged_in?
+    assert_not logged_in?
     assert_redirected_to root_url
     follow_redirect!
     assert_select 'a[href=?]', login_path
