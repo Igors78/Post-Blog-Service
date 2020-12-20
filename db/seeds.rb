7fd064b5e4aac5efd92b97508e7526a84faee8d5
Restyled.io <commits@restyled.io>
2020-12-20 04:55:41 +0000

    Restyled by rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # Create a main sample user.
-User.create!(name:  "Igor Oleynikoff",
-    email: "example@gmail.org",
-    password:              "123456",
-    password_confirmation: "123456",
-    admin: true)
+User.create!(name: 'Igor Oleynikoff',
+             email: 'example@gmail.org',
+             password: '123456',
+             password_confirmation: '123456',
+             admin: true)
 
 # Generate a bunch of additional users.
 99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@gmail.org"
-password = "password"
-User.create!(name:  name,
-      email: email,
-      password:              password,
-      password_confirmation: password)
+  name = Faker::Name.name
+  email = "example-#{n + 1}@gmail.org"
+  password = 'password'
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
 end
