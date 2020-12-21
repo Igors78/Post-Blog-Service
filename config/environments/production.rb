af5fcea605685e093f05dffec378f67a36a532c7
Igors78 <oleinikovsigors@gmail.com>
2020-12-21 14:54:38 +0200

    Add sendgrid integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: ENV['apikey'],
+    password: ENV['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'],
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
