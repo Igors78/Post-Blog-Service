35f1231d31d061dc324a4155fc1103a794da2c63
Igors78 <oleinikovsigors@gmail.com>
2020-12-23 15:59:32 +0200

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_220_184_939) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'password_digest'
-    t.string 'remember_digest'
-    t.boolean 'admin', default: false
-    t.string 'activation_digest'
-    t.boolean 'activated', default: false
-    t.datetime 'activated_at'
-    t.index ['email'], name: 'index_users_on_email', unique: true
+ActiveRecord::Schema.define(version: 2020_12_23_081742) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
