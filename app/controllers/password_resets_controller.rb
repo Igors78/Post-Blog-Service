7de0d92bbb74d2e82eae3ecd5121b49a99b31d64
Igors78 <oleinikovsigors@gmail.com>
2020-12-23 16:34:19 +0200

    Correct valid user

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -44,10 +44,9 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:email])
   end
 
-  # Confirms a valid user.
   def valid_user
-    unless @user&.activated? &&
-           @user&.authenticated?(:reset, params[:id])
+    unless @user && @user.activated? &&
+           @user.authenticated?(:reset, params[:id])
       redirect_to root_url
     end
   end
