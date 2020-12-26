c9f97cb1f7887bc6cf0385a82e1e1e5cf6b6a34a
Igors78 <oleinikovsigors@gmail.com>
2020-12-26 07:40:49 +0200

    Add micropost feed to home

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,6 +22,6 @@ end
 # Generate microposts for a subset of users.
 users = User.order(:created_at).take(6)
 50.times do
-  content = Faker::Lorem.sentence(word_count: 5)
+  content = Faker::ChuckNorris.fact
   users.each { |user| user.microposts.create!(content: content) }
 end
